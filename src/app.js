b3c879ae9890deac9b34d5913678c12cd00f1d0b
Iain Tooley <iaintooley1@gmail.com>
2019-10-21 15:37:07 +0100

    added radio buttons and classes for priorty

--- src/app.js
+++ src/app.js
@@ -5,17 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
     el: "#app",
     data: {
       things: [
-        {what: "Do washing", isDone: true},
-        {what: "Go to the gym", isDone: false},
-        {what: "Eat dinner", isDone: false}
+        {what: "Do washing", isDone: true, priorityLevel: "Low"},
+        {what: "Go to the gym", isDone: false, priorityLevel: "High"},
+        {what: "Eat dinner", isDone: false, priorityLevel: "Low"}
       ],
-      newThing: ""
+      newThing: "",
+      newPriorityLevel: ""
     },
     methods: {
       saveNewThing: function () {
         this.things.push({
           what: this.newThing,
-          isDone: false
+          isDone: false,
+          priorityLevel: this.newPriorityLevel
         });
         this.newThing = "";
       },
