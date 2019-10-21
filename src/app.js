4365af6fa64d84af982885103432a087b7cb9811
Iain Tooley <iaintooley1@gmail.com>
2019-10-21 15:04:07 +0100

    added items and a way to add new ones

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+    data: {
+      things: [
+        {what: "Do washing", isDone: true},
+        {what: "Go to the gym", isDone: false},
+        {what: "Eat dinner", isDone: false}
+      ],
+      newThing: ""
+    },
+    methods: {
+      saveNewThing: function () {
+        this.things.push({
+          what: this.newThing,
+          isDone: false
+        });
+        this.newThing = "";
+      }
+    }
+  });
+});
